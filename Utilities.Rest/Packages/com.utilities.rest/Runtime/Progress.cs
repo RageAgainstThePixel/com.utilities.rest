87199314bf4bb7955948dd016379a227b4c937c1
Stephen Hodgson <hodgson.designs@gmail.com>
2023-03-25 21:51:00 -0400

    com.utilities.rest 1.3.0 (#14)
    - Added Progress member for more detailed reporting
    - Added `StreamAudioAsync` but it may not function as intended until bug is fixed with Unity

---
+++ Utilities.Rest/Packages/com.utilities.rest/Runtime/Progress.cs
@@ -0,0 +1,50 @@
+﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace Utilities.WebRequestRest
+{
+    public readonly struct Progress
+    {
+        public enum DataUnit
+        {
+            /// <summary>
+            /// bytes
+            /// </summary>
+            b,
+            /// <summary>
+            /// kilobytes
+            /// </summary>
+            kB,
+            /// <summary>
+            /// megabytes
+            /// </summary>
+            MB,
+            /// <summary>
+            /// Gigabytes
+            /// </summary>
+            GB,
+            /// <summary>
+            /// Terabytes
+            /// </summary>
+            TB
+        }
+
+        public Progress(ulong position, ulong length, float progress, float speed, DataUnit unit)
+        {
+            Position = position;
+            Length = length;
+            Percentage = progress;
+            Speed = speed;
+            Unit = unit;
+        }
+
+        public ulong Position { get; }
+
+        public ulong Length { get; }
+
+        public float Percentage { get; }
+
+        public float Speed { get; }
+
+        public DataUnit Unit { get; }
+    }
+}
