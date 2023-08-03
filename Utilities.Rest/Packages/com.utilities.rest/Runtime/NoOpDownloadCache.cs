4c54431c09b4eed803742b6cdf02f6d5d4083fa5
Andreas Herbig <andreas.herbig@3spin.com>
2023-08-03 11:47:34 +0200

    Add new download cache that does not cache at all

---
+++ Utilities.Rest/Packages/com.utilities.rest/Runtime/NoOpDownloadCache.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Utilities.WebRequestRest.Interfaces;
+
+namespace Utilities.Rest
+{
+    public class NoOpDownloadCache : IDownloadCache
+    {
+        public void ValidateCacheDirectory()
+        {
+
+        }
+
+        public Task ValidateCacheDirectoryAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public bool TryGetDownloadCacheItem(string uri, out string filePath)
+        {
+            filePath = uri;
+            return false;
+        }
+
+        public bool TryDeleteCacheItem(string uri)
+        {
+            return true;
+        }
+
+        public void DeleteDownloadCache()
+        {
+
+        }
+
+        public Task CacheItemAsync(byte[] data, string cachePath, CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
