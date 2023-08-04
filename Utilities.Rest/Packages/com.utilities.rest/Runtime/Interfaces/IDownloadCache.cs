e52276d98c83786fabd8725fe7c2812d33eeba57
Stephen Hodgson <hodgson.designs@gmail.com>
2023-08-04 16:55:03 -0700

    com.utilities.rest 2.1.2 (#38)
    - better support for Caching in WebGL
    
    ---------
    
    Co-authored-by: Andreas Herbig <andreas.herbig.de@gmail.com>
    Co-authored-by: Andreas Herbig <andreas.herbig@3spin.com>

---
+++ Utilities.Rest/Packages/com.utilities.rest/Runtime/Interfaces/IDownloadCache.cs
@@ -0,0 +1,22 @@
+﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Utilities.WebRequestRest.Interfaces
+{
+    internal interface IDownloadCache
+    {
+        void ValidateCacheDirectory();
+
+        Task ValidateCacheDirectoryAsync();
+
+        bool TryGetDownloadCacheItem(string uri, out string filePath);
+
+        bool TryDeleteCacheItem(string uri);
+
+        void DeleteDownloadCache();
+
+        Task WriteCacheItemAsync(byte[] data, string cachePath, CancellationToken cancellationToken);
+    }
+}
