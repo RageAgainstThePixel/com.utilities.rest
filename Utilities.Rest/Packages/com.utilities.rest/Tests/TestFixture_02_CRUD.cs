f953ad40b05dffe5f5c27de25eecbca6089b5e0a
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-10-31 21:22:45 -0400

    com.utilities.rest 5.0.1
    - propagate task cancellation exception when using cancellation token cancel call

--- Utilities.Rest/Packages/com.utilities.rest/Tests/TestFixture_02_CRUD.cs
+++ Utilities.Rest/Packages/com.utilities.rest/Tests/TestFixture_02_CRUD.cs
@@ -6,6 +6,7 @@ using System;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.TestTools;
 
 namespace Utilities.WebRequestRest.Tests
 {
@@ -21,13 +22,12 @@ namespace Utilities.WebRequestRest.Tests
             {
                 using var cts = new CancellationTokenSource();
                 cts.CancelAfter(TimeSpan.FromSeconds(5));
-                var response = await Rest.GetAsync(SseServer, ServerSentEventHandler, cancellationToken: cts.Token);
-                Assert.IsTrue(response.Successful);
-                response.Validate(true);
+                await Rest.GetAsync(SseServer, ServerSentEventHandler, cancellationToken: cts.Token);
 
                 Task ServerSentEventHandler(Response res, ServerSentEvent sse)
                 {
                     Debug.Log(sse.ToJsonString());
+                    Assert.IsTrue(res.Successful);
                     res.Validate(true);
 
                     return Task.CompletedTask;
@@ -35,9 +35,17 @@ namespace Utilities.WebRequestRest.Tests
             }
             catch (Exception e)
             {
-                Debug.LogException(e);
+                switch (e)
+                {
+                    case TaskCanceledException:
+                    case OperationCanceledException:
+                        // expected due to cancellation token
+                        break;
+                    default:
+                        Debug.LogException(e);
+                        throw;
 
-                throw;
+                }
             }
         }
 
@@ -47,8 +55,8 @@ namespace Utilities.WebRequestRest.Tests
             try
             {
                 var response = await Rest.GetAsync("https://jsonplaceholder.typicode.com/posts/1");
-                Assert.IsTrue(response.Successful);
                 response.Validate(true);
+                Assert.IsTrue(response.Successful);
             }
             catch (Exception e)
             {
@@ -64,8 +72,8 @@ namespace Utilities.WebRequestRest.Tests
             {
                 var payload = new { title = "foo", body = "bar", userId = 1 };
                 var response = await Rest.PostAsync("https://jsonplaceholder.typicode.com/posts", JsonConvert.SerializeObject(payload));
-                Assert.IsTrue(response.Successful);
                 response.Validate(true);
+                Assert.IsTrue(response.Successful);
             }
             catch (Exception e)
             {
@@ -81,8 +89,8 @@ namespace Utilities.WebRequestRest.Tests
             {
                 var payload = new { id = 1, title = "foo", body = "bar", userId = 1 };
                 var response = await Rest.PutAsync("https://jsonplaceholder.typicode.com/posts/1", JsonConvert.SerializeObject(payload));
-                Assert.IsTrue(response.Successful);
                 response.Validate(true);
+                Assert.IsTrue(response.Successful);
             }
             catch (Exception e)
             {
@@ -98,8 +106,8 @@ namespace Utilities.WebRequestRest.Tests
             {
                 var payload = new { title = "foo" };
                 var response = await Rest.PatchAsync("https://jsonplaceholder.typicode.com/posts/1", JsonConvert.SerializeObject(payload));
-                Assert.IsTrue(response.Successful);
                 response.Validate(true);
+                Assert.IsTrue(response.Successful);
             }
             catch (Exception e)
             {
@@ -114,8 +122,8 @@ namespace Utilities.WebRequestRest.Tests
             try
             {
                 var response = await Rest.DeleteAsync("https://jsonplaceholder.typicode.com/posts/1");
-                Assert.IsTrue(response.Successful);
                 response.Validate(true);
+                Assert.IsTrue(response.Successful);
             }
             catch (Exception e)
             {
