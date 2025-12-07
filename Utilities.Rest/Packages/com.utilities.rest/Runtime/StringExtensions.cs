ecd89cae2a061b6b4dcdc4d2def581e42ac2a970
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2025-12-06 19:46:23 -0500

    com.utilities.rest 5.1.0 (#112)
    - Refactored all cache-related methods in `IDownloadCache`, `DiskDownloadCache`, and `NoOpDownloadCache` to use `Uri` objects instead of strings for URIs and file paths, improving type safety and consistency.
      - Refactor `fileName` calculations to improve performance
      - Skips `UnityWebRequest.UnEscapeURL` if loading from local resource
      - Updated cache item lookup, deletion, and writing logic to operate on `Uri.LocalPath` and handle file existence checks and file operations using the `Uri` type.
    - `Rest.TryGetFileNameFromUrl` will return false if the uri is not a remote resource
    - Replaced `Rest.TryGetFileNameFromUrl` with `Rest.TryGetFileNameFromUri`
    - Add `Uri` request overloads for all CRUD operations

---
+++ Utilities.Rest/Packages/com.utilities.rest/Runtime/StringExtensions.cs
@@ -0,0 +1,50 @@
+﻿// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Utilities.WebRequestRest
+{
+    internal static class StringExtensions
+    {
+        /// <summary>
+        /// Generates a <see cref="Guid"/> based on the string.
+        /// </summary>
+        /// <param name="string">
+        /// The string to generate the <see cref="Guid"/>.
+        /// </param>
+        /// <returns>
+        /// A new <see cref="Guid"/> that represents the string.
+        /// </returns>
+        public static Guid GenerateGuid(string @string)
+        {
+            using MD5 md5 = MD5.Create();
+            return new Guid(md5.ComputeHash(Encoding.UTF8.GetBytes(@string)));
+        }
+
+        /// <summary>
+        /// Generates a string representation of a <see cref="Guid"/> based on the string.
+        /// </summary>
+        /// <param name="string">
+        /// The string to generate the <see cref="Guid"/>.
+        /// </param>
+        /// <returns>
+        /// A new string that represents the <see cref="Guid"/> generated from the string.
+        /// </returns>
+        public static string GenerateGuidString(string @string)
+            => GenerateGuid(@string).ToString();
+
+        /// <summary>
+        /// Generates a string representation of a <see cref="Guid"/> based on the <see cref="Uri"/>.
+        /// </summary>
+        /// <param name="uri">
+        /// The <see cref="Uri"/> to generate the <see cref="Guid"/> string from.
+        /// </param>
+        /// <returns>
+        /// A new string that represents the <see cref="Guid"/> generated from the <see cref="Uri"/>.
+        /// </returns>
+        public static string GenerateGuidString(this Uri uri)
+            => GenerateGuidString(uri.ToString());
+    }
+}
